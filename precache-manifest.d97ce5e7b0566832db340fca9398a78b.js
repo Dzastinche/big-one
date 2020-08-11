b3b27daff6b7edcc85b785843472485a883196c7
Dzastinche <63582403+Dzastinche@users.noreply.github.com>
2020-08-11 23:41:58 +0200

    Updates

---
+++ precache-manifest.d97ce5e7b0566832db340fca9398a78b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ffa66d758d69f5bafd36b6f310a986e",
+    "url": "/Dzastinche/bigOne/index.html"
+  },
+  {
+    "revision": "4d9449dee59add4a7943",
+    "url": "/Dzastinche/bigOne/static/css/main.bac8d025.chunk.css"
+  },
+  {
+    "revision": "cb4fa182608fe40b2df6",
+    "url": "/Dzastinche/bigOne/static/js/2.a2999daa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dzastinche/bigOne/static/js/2.a2999daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9449dee59add4a7943",
+    "url": "/Dzastinche/bigOne/static/js/main.4048e1ec.chunk.js"
+  },
+  {
+    "revision": "27af09620ea788a8ab7e",
+    "url": "/Dzastinche/bigOne/static/js/runtime-main.0d159b29.js"
+  }
+]);
